26b11f97c50bb9cac30b3da65da3fe3582d3def0
rozer4heros <rozer4heros@gmail.com>
2026-06-25 15:48:39 +0900

    feat: 컴포넌트 뼈대 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header.jsx";
+import Nav from "./components/Nav.jsx";
+import Figure from "./components/Figure.jsx";
 
 function App() {
   return (
     <>
-      <></>
+      <Header />
+      <Nav />
+      <Figure />
     </>
   );
 }
